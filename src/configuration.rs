549b814c54044849066e14eaef23aed643df1831
Jeremy Borgman <borgman.jeremy@pm.me>
2020-12-27 14:25:40 -0600

    added simple postgres (finished chapter 3.5)

--- src/configuration.rs
+++ src/configuration.rs
@@ -13,6 +13,15 @@ impl DatabaseSettings {
                 self.port,
                 self.database_name)
     }
+
+    pub fn connection_string_without_db(&self) -> String {
+        format!("postgres://{}:{}@{}:{}",
+                self.username,
+                self.password,
+                self.host,
+                self.port
+        )
+    }
 }
 
 #[derive(serde::Deserialize)]
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,17 @@ use rss::startup::run;
 use std::net::TcpListener;
 use rss::configuration::get_configuration;
 
+use sqlx::PgPool;
+
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to postgres.");
 
-    let address = format!("127.0.0.1:{}",configuration.application_port) ;
+    let address = format!("127.0.0.1:{}", configuration.application_port);
 
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
